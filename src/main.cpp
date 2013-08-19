c36d1d2f46d3435c58e8c325a769672a0e9380b2
Gregory Maxwell <greg@xiph.org>
2013-08-18 20:21:06 -0700

    Performance optimization for bloom filters.
    This reduces a peer's ability to attack network resources by
     using a full bloom filter, but without reducing the usability
     of bloom filters.  It sets a default match everything filter
     for peers and it generalizes a prior optimization to
     cover more cases.

--- src/main.cpp
+++ src/main.cpp
@@ -3691,6 +3691,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             LOCK(pfrom->cs_filter);
             delete pfrom->pfilter;
             pfrom->pfilter = new CBloomFilter(filter);
+            filter.UpdateEmptyFull();
         }
         pfrom->fRelayTxes = true;
     }
@@ -3720,7 +3721,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
     {
         LOCK(pfrom->cs_filter);
         delete pfrom->pfilter;
-        pfrom->pfilter = NULL;
+        pfrom->pfilter = new CBloomFilter();
         pfrom->fRelayTxes = true;
     }
 
