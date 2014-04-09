9b33cc4882b3db8e0411daca2b76188c9163a2aa
Cory Fields <cory-nospam-@coryfields.com>
2014-04-09 01:45:21 -0400

    build: use baked-in fdelt_warn rather than leaving an unresolved weak symbol

--- src/glibc_compat.cpp
+++ src/glibc_compat.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <sys/select.h>
 extern "C" void* memmove(void* a, const void* b, size_t c);
 extern "C" void* __real_memcpy(void* a, const void* b, size_t c) __attribute__((weak));
 extern "C" void* __wrap_memcpy(void* a, const void* b, size_t c)
@@ -8,11 +9,13 @@ extern "C" void* __wrap_memcpy(void* a, const void* b, size_t c)
   return memmove(a, b, c);
 }
 
+extern "C" void __chk_fail (void) __attribute__((__noreturn__));
 extern "C" long int __real___fdelt_warn(long int) __attribute__((weak));
 extern "C" long int __wrap___fdelt_warn(long int a)
 {
-  if(__real___fdelt_warn)
-    return __real___fdelt_warn(a);
-  return 0;
+  if (a >= FD_SETSIZE)
+    __chk_fail ();
+
+  return a / __NFDBITS;
 }
 extern "C" long int __fdelt_chk(long int) __attribute__((weak, alias("__wrap___fdelt_warn")));
