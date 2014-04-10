293fbda294667d023528049309179dc66d55e9e8
Cory Fields <cory-nospam-@coryfields.com>
2014-04-09 20:08:07 -0400

    build: another round of glibc/libstdc++ hacking
    Now with libstdc++ compat back to GLIBCXX_3.4.

--- src/glibc_compat.cpp
+++ src/glibc_compat.cpp
@@ -1,5 +1,7 @@
+#include "bitcoin-config.h"
 #include <cstddef>
 #include <sys/select.h>
+
 extern "C" void* memmove(void* a, const void* b, size_t c);
 extern "C" void* __real_memcpy(void* a, const void* b, size_t c) __attribute__((weak));
 extern "C" void* __wrap_memcpy(void* a, const void* b, size_t c)
@@ -10,12 +12,12 @@ extern "C" void* __wrap_memcpy(void* a, const void* b, size_t c)
 }
 
 extern "C" void __chk_fail (void) __attribute__((__noreturn__));
-extern "C" long int __real___fdelt_warn(long int) __attribute__((weak));
-extern "C" long int __wrap___fdelt_warn(long int a)
+extern "C" FDELT_TYPE __real___fdelt_warn(FDELT_TYPE) __attribute__((weak));
+extern "C" FDELT_TYPE __wrap___fdelt_warn(FDELT_TYPE a)
 {
   if (a >= FD_SETSIZE)
     __chk_fail ();
 
   return a / __NFDBITS;
 }
-extern "C" long int __fdelt_chk(long int) __attribute__((weak, alias("__wrap___fdelt_warn")));
+extern "C" FDELT_TYPE __fdelt_chk(FDELT_TYPE) __attribute__((weak, alias("__wrap___fdelt_warn")));
